d254f0990b60d7a9d52719664ffb6791482d8e9e
Jared Key <key.jared.a@gmail.com>
2025-03-12 20:23:57 +0100

    passed part 1

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,57 @@
 package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	Db_url            string `json:"db_url"`
+	Current_user_name string `json:"current_user_name"`
+}
+
+func getFileName() string {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println("filed to get user home dir")
+		return ""
+	}
+
+	filename := home + "/.gatorconfig.json"
+	return filename
+}
+
+func Read() Config {
+	filename := getFileName()
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Printf("failed to read file: %v\n", filename)
+		return Config{}
+	}
+
+	var config Config
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		fmt.Println("failed to unmarshal data from file")
+		return Config{}
+	}
+
+	return config
+}
+
+func (c *Config) SetUser(username string) {
+	c.Current_user_name = username
+
+	json_data, err := json.Marshal(c)
+	if err != nil {
+		fmt.Println("failed to marshal data")
+		return
+	}
+	filename := getFileName()
+	err = os.WriteFile(filename, json_data, 0644)
+	if err != nil {
+		fmt.Printf("failed to write file with err: %v", err)
+		return
+	}
+}
--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/mrjkey/aggregator/internal/config"
+)
+
+func main() {
+	gator_config := config.Read()
+	gator_config.SetUser("jared")
+	gator_config_2 := config.Read()
+	fmt.Println(gator_config_2)
 }
